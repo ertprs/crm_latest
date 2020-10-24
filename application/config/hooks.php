bb7a8f2f1604f77aba2fe15b5c243e03ad03a3dc
sandhya0712 <sandyam190@gmail.com>
2020-10-24 08:18:37 +0800

    sandhya removed
    old file

--- application/config/hooks.php
+++
@@ -1,9 +0,0 @@
-<?php
-defined( 'BASEPATH' )OR exit( 'No direct script access allowed' );
-
-$hook['post_controller_constructor'][] = array(
-	'class'    => 'Remote_Check',
-	'function' => 'index',
-	'filename' => 'App_Config.php',
-	'filepath' => 'hooks'
-);
